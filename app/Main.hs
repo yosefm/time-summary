0a02e3869a3e40c4987bc2f06d6a97cce790d840
Yosef Meller <yosef.meller@intusurg.com>
2024-07-24 17:00:59 +0300

    Move most code to Lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,53 +1,9 @@
 module Main (main) where
 
-import Control.Monad
-import Data.Char (isSpace, toLower)
 import Data.Maybe
-import Data.List
-
 import Data.Time
-import Data.Time.Format
 
 import Lib
-
-type Clock = LocalTime
-data DayContent = Vacation | SickLeave | Unknown
-     | Worked Clock Clock
-  deriving Show
-
-data WorkDay = WorkDay {
-    theDate :: LocalTime,
-    content :: DayContent
-  }
-  deriving Show
-
-trim :: String -> String
-trim = dropWhileEnd isSpace . dropWhile isSpace
-
-parseWorkRange :: String -> Maybe DayContent
-parseWorkRange s = 
-    let (entryTimeStr, exitTimeStr) = break isSpace (dropWhile isSpace s)
-        parseIt = parseTimeM True defaultTimeLocale "%k:%M"
-        entryTime = parseIt entryTimeStr
-        exitTime = parseIt $ takeWhile (not . isSpace) $ trim exitTimeStr
-    in 
-        liftA2 Worked entryTime exitTime
-
-parseDayContent :: String -> DayContent
-parseDayContent s
-    | lower s == "vacation"  = Vacation
-    | lower s == "sick day"  = SickLeave
-    | otherwise = maybe Unknown id (parseWorkRange s)
-    
-  where lower = map toLower
-  
-parseWorkDay :: String -> Maybe WorkDay
-parseWorkDay s =
-    let (dateStr, rest) = break isSpace s
-        parsedDate = parseTimeM True defaultTimeLocale "%e.%-m" dateStr
-        retDay d = WorkDay d $ parseDayContent rest
-    in 
-        retDay <$> parsedDate
 
 main :: IO ()
 main = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,44 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib ( WorkDay(..), DayContent, parseWorkDay ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char (isSpace, toLower)
+import Data.List (dropWhileEnd)
+import Data.Time
+
+type Clock = LocalTime
+data DayContent = Vacation | SickLeave | Unknown
+     | Worked Clock Clock
+  deriving Show
+
+data WorkDay = WorkDay {
+    theDate :: LocalTime,
+    content :: DayContent
+  }
+  deriving Show
+
+trim :: String -> String
+trim = dropWhileEnd isSpace . dropWhile isSpace
+
+parseWorkRange :: String -> Maybe DayContent
+parseWorkRange s = 
+    let (entryTimeStr, exitTimeStr) = break isSpace (dropWhile isSpace s)
+        parseIt = parseTimeM True defaultTimeLocale "%k:%M"
+        entryTime = parseIt entryTimeStr
+        exitTime = parseIt $ takeWhile (not . isSpace) $ trim exitTimeStr
+    in 
+        liftA2 Worked entryTime exitTime
+
+parseDayContent :: String -> DayContent
+parseDayContent s
+    | lower s == "vacation"  = Vacation
+    | lower s == "sick day"  = SickLeave
+    | otherwise = maybe Unknown id (parseWorkRange s)
+    
+  where lower = map toLower
+  
+parseWorkDay :: String -> Maybe WorkDay
+parseWorkDay s =
+    let (dateStr, rest) = break isSpace s
+        parsedDate = parseTimeM True defaultTimeLocale "%e.%-m" dateStr
+        retDay d = WorkDay d $ parseDayContent rest
+    in 
+        retDay <$> parsedDate
