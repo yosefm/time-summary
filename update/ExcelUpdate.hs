d31eaaa2a025a350c503a4f43f4e8900c6afed9e
Yosef Meller <yosef.meller@intusurg.com>
2024-10-22 18:18:54 +0300

    Get building with Excel package

--- update/ExcelUpdate.hs
+++ update/ExcelUpdate.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
+import Data.Maybe
+import Data.Time
+
+import Codec.Xlsx
 import Lib
 
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+    fileContent <- readFile "/mnt/c/Users/ymeller/presence.txt"
+    
+    let getMonth wd = case (toGregorian . localDay . theDate) wd of
+            (_, m, _) -> m
+        monthWorkedDays = filter (( == 10) . getMonth) $ mapMaybe parseWorkDay $ lines fileContent
+        -- updateSheet = 
+    
+    return ()
+        
