bcd54814e437e27fcf88626d3466b80deeb18ab7
Yosef Meller <yosef.meller@intusurg.com>
2024-11-04 11:06:49 +0200

    Modify name and write output

--- update/ExcelUpdate.hs
+++ update/ExcelUpdate.hs
@@ -7,6 +7,7 @@ import Data.Maybe
 import Data.Time
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as L
+import Data.Time.Clock.POSIX
 
 import Codec.Xlsx
 import Control.Lens
@@ -18,20 +19,28 @@ validateFormat sheet =
         toText (CellText txt) = txt
         toText _ = ""
     in maybe False ((== "Date ") . toText) dateHeaderCell
+    
+putName :: Worksheet -> T.Text -> Worksheet
+putName sheet name = sheet & cellValueAt (2, 3) ?~ CellText name
 
 main :: IO ()
 main = do
     fileContent <- readFile "/mnt/c/Users/ymeller/presence.txt"
     srcXlsFile <- L.readFile "/mnt/c/Users/ymeller/Documents/Administrata/Monthly attendance report October 2024.xlsx"
+    ct <- getPOSIXTime    
     
     let getMonth wd = case (toGregorian . localDay . theDate) wd of
             (_, m, _) -> m
         monthWorkedDays = filter (( == 10) . getMonth) $ mapMaybe parseWorkDay $ lines fileContent
-        sheet = toXlsx srcXlsFile ^? ixSheet "Sheet1"
+        srcXlsx = toXlsx srcXlsFile 
+        sheet = srcXlsx ^? ixSheet "Sheet1"
+        updatedXlsx newSheet = srcXlsx & atSheet "Sheet1" ?~ newSheet
     
-    case validateFormat <$> sheet of 
-        Nothing -> putStrLn "Invalid template file: meesing worksheet"
-        Just False -> putStrLn "Invalid template file: required date not found"
-        Just True -> putStrLn "Awesome template file"
+    case sheet of 
+        Nothing -> putStrLn "Invalid template file: missing worksheet"
+        Just s -> 
+            case validateFormat s of 
+                False -> putStrLn "Invalid template file: required date header not found"
+                True -> L.writeFile "output.xlsx" (fromXlsx ct $ updatedXlsx $ putName s "Yosef Meller")
         
     
